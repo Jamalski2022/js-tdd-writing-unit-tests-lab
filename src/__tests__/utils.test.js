752ad3276ff0e6d8c5778537890ecf8143af6644
Jamal Ahmed <jamal.ahmed@student.moringaschool.com>
2024-09-11 10:33:08 +0300

    complete lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,20 @@
 // Your tests here
+
+const { currentAgeForBirthYear } = require('../utils');
+
+
+describe('currentAgeForBirthYear', () => {
+
+
+    it('returns the age of a person based on the year of birth', () => {
+        const birthYear = 1984;
+        const currentYear = new Date().getFullYear();
+        const expectedAge = currentYear - birthYear;
+
+        const ageOfPerson = currentAgeForBirthYear(birthYear);
+
+
+        expect(ageOfPerson).toBe(expectedAge);
+    });
+
+});
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,9 @@
 // Your code here
+
+
+function currentAgeForBirthYear(birthYear) {
+    const currentYear = new Date().getFullYear();
+    return currentYear - birthYear;
+}
+
+module.exports = { currentAgeForBirthYear };  
